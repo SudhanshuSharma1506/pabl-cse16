4042680f7ef98a9ef743d979d4bc50ac95086732
sudhanshu sharma <sudhanshusharma1906@gmail.com>
2026-01-24 22:40:43 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,15 +1,1 @@
-class Solution:
-    def reverseArray(self, arr):
-        left, right = 0, len(arr) - 1
-        
-        while left < right:
-            arr[left], arr[right] = arr[right], arr[left]
-            left += 1
-            right -= 1
-        
-        return arr
 
-
-arr = [1, 2, 3, 4, 5, 7]
-obj = Solution()
-print(obj.reverseArray(arr))
